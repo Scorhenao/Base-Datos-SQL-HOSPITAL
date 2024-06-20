f5922de90ca5a2bb2f125fc06dcd273552302abb
scorhenao <samcorhenao@gmail.com>
2024-06-20 19:19:49 -0400

    adding tables RAZONES_DE_INGRESO, ALERGIAS and PACIENTES_ALERGIAS

--- app/module/db/db.sql
+++ app/module/db/db.sql
@@ -15,6 +15,7 @@ CREATE TABLE CONTACTOS_DE_EMERGENCIA (
     Telefono VARCHAR(16) NOT NULL COMMENT 'Telefono de contacto de emergencia'
 ) COMMENT 'Tabla de CONTACTOS_DE_EMERGENCIA';
 
+
 /*table PACIENTES*/
 CREATE TABLE PACIENTES (  
     ID_paciente INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key de PACIENTES',
@@ -27,3 +28,27 @@ CREATE TABLE PACIENTES (
     ID_contactoDeEmergencia_FK INT,
     FOREIGN KEY(ID_contactoDeEmergencia_FK) REFERENCES CONTACTOS_DE_EMERGENCIA(ID_contactoDeEmergencia)
 ) COMMENT 'Tabla de PACIENTES';
+
+/*table RAZONES_DE_INGRESO*/
+CREATE TABLE RAZONES_DE_INGRESO (
+    ID_razonDeIngreso INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key de RAZONES_DE_INGRESO',
+    Observacion VARCHAR(250) COMMENT 'Observacion del medico deacuerdo a la razon de ingreso del paciente',
+    ID_paciente_FK INT NOT NULL COMMENT 'Foreign key del paciente',
+    FOREIGN KEY(ID_paciente_FK) REFERENCES PACIENTES(ID_paciente)
+)COMMENT 'tabla de RAZONES_DE_INGRESO';
+
+/*table ALERGIAS*/
+CREATE TABLE ALERGIAS (
+    ID_alergia INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key de ALERGIAS',
+    NombreDeAlergia TEXT(50) NOT NULL COMMENT 'Nombre de la alergia',
+    TipoDeAlergia TEXT(50) NOT NULL COMMENT 'Tipo de la alergia'
+)COMMENT 'tabla de ALERGIAS';
+
+/*table PACIENTES_ALERGIAS*/
+CREATE TABLE PACIENTES_ALERGIAS (
+    ID_paciente_FK INT NOT NULL COMMENT 'Foreign key del paciente',
+    FOREIGN KEY(ID_paciente_FK) REFERENCES PACIENTES(ID_paciente),
+    ID_alergia_FK INT NOT NULL COMMENT 'Foreign key de ALERGIAS',
+    FOREIGN KEY(ID_alergia_FK) REFERENCES ALERGIAS(ID_alergia)
+)COMMENT 'tabla de PACIENTES_ALERGIAS';
+
