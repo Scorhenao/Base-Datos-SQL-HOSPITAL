d24dc038c72cdeee08dfe3c9addd5005667ce6a1
scorhenao <samcorhenao@gmail.com>
2024-06-23 12:49:59 -0500

    adding null to FK  if the Estado = Disponible

--- app/module/insertions/ALL_INSERTIONS.sql
+++ app/module/insertions/ALL_INSERTIONS.sql
@@ -280,26 +280,27 @@ VALUES
 -- INSERT CAMAS
 INSERT INTO CAMAS (Estado,ID_paciente_FK, ID_salaUCI_FK)
 VALUES
-    ('Disponible',1,1),    -- María González
+    ('Disponible',NULL,1),    -- María González
     ('Ocupado',2,2),    -- Pedro Ramírez
-    ('Disponible',3,3),    -- Laura Martínez
+    ('Disponible',NULL,3),    -- Laura Martínez
     ('Ocupado',4,4),    -- Carlos Sánchez
-    ('Disponible',5,5),    -- Ana López
+    ('Disponible',NULL,5),    -- Ana López
     ('Ocupado',6,6),    -- Jorge Rodríguez
-    ('Disponible',7,7),    -- Sofía Fernández
+    ('Disponible',NULL,7),    -- Sofía Fernández
     ('Ocupado',8,8),    -- Miguel Torres
-    ('Disponible',9,9),    -- Elena Gómez
+    ('Disponible',NULL,9),    -- Elena Gómez
     ('Ocupado',10,10),   -- Diego García
-    ('Disponible',11,11),   -- Patricia Ruiz
+    ('Disponible',NULL,11),   -- Patricia Ruiz
     ('Ocupado',12,12),   -- Ricardo Vargas
-    ('Disponible',13,13),   -- Alejandra Pérez
+    ('Disponible',NULL,13),   -- Alejandra Pérez
     ('Ocupado',14,14),   -- Mario Hernández
-    ('Disponible',15,15),   -- Luisa Ramírez
+    ('Disponible',NULL,15),   -- Luisa Ramírez
     ('Ocupado',16,16),   -- Andrés Gutiérrez
-    ('Disponible',17,17),   -- Fabiola Díaz
+    ('Disponible',NULL,17),   -- Fabiola Díaz
     ('Ocupado',18,18),   -- Roberto Castro
-    ('Disponible',19,19),   -- Claudia Morales
+    ('Disponible',NULL,19),   -- Claudia Morales
     ('Ocupado',20,20);   -- Ana María López
+
 -- INSERT ENFERMERAS
 INSERT INTO ENFERMERAS (Nombre, Apellidos, TelefonoDeContacto, FechaDeNacimiento, NumeroDeDocumento, AniosDeExperiencia, ID_genero_FK, ID_tipoDeDocumento_FK, ID_contrato_FK, `ID_salaUCI_FK`)
 VALUES
