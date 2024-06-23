a9afe1e26cbf9f479411b48429fb08ec150491ea
scorhenao <samcorhenao@gmail.com>
2024-06-23 11:34:41 -0500

    ej 3 enfermeras ready

--- app/consults/enfermeras/3Enfermera.sql
+++ app/consults/enfermeras/3Enfermera.sql
@@ -1,5 +1,6 @@
 - Dado una identificación de un paciente, conocer los exámenes realizados.
-SELECT EXAMENES.ID_examen, RAZONES_DE_INGRESO.Observacion, PACIENTES.Nombre, PACIENTES.Apellidos, PACIENTES.NumeroDeDocumento/*Table where u are and table in which u wanna search*/
-FROM RAZONES_DE_INGRESO
-JOIN PACIENTES ON RAZONES_DE_INGRESO.ID_paciente_FK = PACIENTES.ID_paciente /*Compare if the FK is the same with the ID of the real table*/
-WHERE PACIENTES.NumeroDeDocumento = 23456789012;/*The document that that we need*/
+SELECT EXAMENES.ID_examen, EXAMENES.Nombre, EXAMENES.Fecha, PACIENTES.Nombre, PACIENTES.Apellidos, PACIENTES.NumeroDeDocumento,MEDICOS.Nombre,MEDICOS.Apellidos/*Table where u are and table in which u wanna search*/
+FROM EXAMENES
+JOIN PACIENTES ON EXAMENES.ID_paciente_FK = PACIENTES.ID_paciente /*Compare if the FK is the same with the ID of the real table*/
+JOIN MEDICOS ON EXAMENES.ID_medico_FK = MEDICOS.ID_medico /*Compare if the FK is the same with the ID of the real table*/
+WHERE PACIENTES.NumeroDeDocumento = 21098765432;/*The document that that we need*/
