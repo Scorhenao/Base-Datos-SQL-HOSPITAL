88ed4f886b2f01f48f408341e8bad4fb134aecfe
scorhenao <samcorhenao@gmail.com>
2024-06-23 12:31:50 -0500

    new insertion in CAMAS  "Estado"

--- app/module/insertions/CAMAS.sql
+++ app/module/insertions/CAMAS.sql
@@ -1,23 +1,23 @@
 -- Insertar registros en la tabla CAMAS
-INSERT INTO CAMAS (ID_paciente_FK, ID_salaUCI_FK)
+INSERT INTO CAMAS (Estado,ID_paciente_FK, ID_salaUCI_FK)
 VALUES
-    (1,1),    -- María González
-    (2,2),    -- Pedro Ramírez
-    (3,3),    -- Laura Martínez
-    (4,4),    -- Carlos Sánchez
-    (5,5),    -- Ana López
-    (6,6),    -- Jorge Rodríguez
-    (7,7),    -- Sofía Fernández
-    (8,8),    -- Miguel Torres
-    (9,9),    -- Elena Gómez
-    (10,10),   -- Diego García
-    (11,11),   -- Patricia Ruiz
-    (12,12),   -- Ricardo Vargas
-    (13,13),   -- Alejandra Pérez
-    (14,14),   -- Mario Hernández
-    (15,15),   -- Luisa Ramírez
-    (16,16),   -- Andrés Gutiérrez
-    (17,17),   -- Fabiola Díaz
-    (18,18),   -- Roberto Castro
-    (19,19),   -- Claudia Morales
-    (20,20);   -- Ana María López
+    ('Disponible',1,1),    -- María González
+    ('Ocupado',2,2),    -- Pedro Ramírez
+    ('Disponible',3,3),    -- Laura Martínez
+    ('Ocupado',4,4),    -- Carlos Sánchez
+    ('Disponible',5,5),    -- Ana López
+    ('Ocupado',6,6),    -- Jorge Rodríguez
+    ('Disponible',7,7),    -- Sofía Fernández
+    ('Ocupado',8,8),    -- Miguel Torres
+    ('Disponible',9,9),    -- Elena Gómez
+    ('Ocupado',10,10),   -- Diego García
+    ('Disponible',11,11),   -- Patricia Ruiz
+    ('Ocupado',12,12),   -- Ricardo Vargas
+    ('Disponible',13,13),   -- Alejandra Pérez
+    ('Ocupado',14,14),   -- Mario Hernández
+    ('Disponible',15,15),   -- Luisa Ramírez
+    ('Ocupado',16,16),   -- Andrés Gutiérrez
+    ('Disponible',17,17),   -- Fabiola Díaz
+    ('Ocupado',18,18),   -- Roberto Castro
+    ('Disponible',19,19),   -- Claudia Morales
+    ('Ocupado',20,20);   -- Ana María López
