d0741c3e10fc3417575609d9c39f70c886c9db4e
scorhenao <samcorhenao@gmail.com>
2024-06-23 14:42:44 -0500

    ej 3 admin READY

--- app/consults/administradores/3Admin.sql
+++ app/consults/administradores/3Admin.sql
@@ -1,1 +1,11 @@
 - Dado una identificación de un médico, conocer sus datos personales y salario.
+SELECT MEDICOS.ID_medico, MEDICOS.Nombre, MEDICOS.Apellidos, MEDICOS.TelefonoDeContacto, MEDICOS.FechaDeNacimiento,MEDICOS.NumeroDeDocumento,
+TITULOS.Nombre AS Nombre_Del_Titulo,
+GENEROS.Nombre AS Nombre_Del_Genero,
+TIPOS_DE_DOCUMENTOS.Nombre AS Tipo_Del_Documento,
+CONTRATOS.Salario AS Salario_Del_Medico/*Table where u are and table in which u wanna search*/
+FROM MEDICOS
+JOIN TITULOS ON MEDICOS.ID_titulo_FK = TITULOS.ID_titulo /*Compare if the FK is the same with the ID of the real table*/
+JOIN GENEROS ON MEDICOS.ID_genero_FK = GENEROS.ID_genero /*Compare if the FK is the same with the ID of the real table*/
+JOIN TIPOS_DE_DOCUMENTOS ON MEDICOS.ID_tipoDeDocumento_FK = TIPOS_DE_DOCUMENTOS.ID_tipoDeDocumento /*Compare if the FK is the same with the ID of the real table*/
+JOIN CONTRATOS ON MEDICOS.ID_contrato_FK = CONTRATOS.ID_contrato /*Compare if the FK is the same with the ID of the real table*/
