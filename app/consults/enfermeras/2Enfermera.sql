127d431649ce04ad9ae97f4fa5d35c6d3c0d39b9
scorhenao <samcorhenao@gmail.com>
2024-06-23 11:05:46 -0500

    ej2 enfermera ready

--- app/consults/enfermeras/2Enfermera.sql
+++ app/consults/enfermeras/2Enfermera.sql
@@ -1,1 +1,6 @@
 - Dado una identificación de un paciente, conocer sus motivos de ingreso a la UCI.
+
+SELECT RAZONES_DE_INGRESO.ID_razonDeIngreso, RAZONES_DE_INGRESO.Observacion, PACIENTES.Nombre, PACIENTES.Apellidos, PACIENTES.NumeroDeDocumento/*Table where u are and table in which u wanna search*/
+FROM RAZONES_DE_INGRESO
+JOIN PACIENTES ON RAZONES_DE_INGRESO.ID_paciente_FK = PACIENTES.ID_paciente /*Compare if the FK is the same with the ID of the real table*/
+WHERE PACIENTES.NumeroDeDocumento = 23456789012;/*The document that that we need*/
