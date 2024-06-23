a1d6427ad12178cb8f493a8d120af0a6104d9d0a
scorhenao <samcorhenao@gmail.com>
2024-06-23 12:20:00 -0500

    ej5 enfermera READY

--- app/consults/enfermeras/5Enfermera.sql
+++ app/consults/enfermeras/5Enfermera.sql
@@ -1,1 +1,4 @@
 - Dado el nombre de un medicamento, conocer sus características.
+SELECT MEDICAMENTOS.ID_codigo,MEDICAMENTOS.Dosis/*Table where u are and table in which u wanna search*/
+FROM MEDICAMENTOS /*from medicamentos we gonna search*/
+WHERE MEDICAMENTOS.Nombre = 'Ibuprofeno';/*The name of the medicamento*/
