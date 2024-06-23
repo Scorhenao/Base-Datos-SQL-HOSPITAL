16b008882223c089aaf42012ad76cf0aa0e45601
scorhenao <samcorhenao@gmail.com>
2024-06-23 14:59:34 -0500

    ej 5 admin READY

--- app/consults/administradores/5Admin.sql
+++ app/consults/administradores/5Admin.sql
@@ -1,1 +1,14 @@
 - Dada una sala, conocer cuántas camas están disponibles/ocupadas.
+SELECT 
+    SALAS_UCI.ID_salaUCI,
+    COUNT(CASE WHEN CAMAS.Estado = 'ocupado' THEN 1 END) AS Ocupadas,
+    COUNT(CASE WHEN CAMAS.Estado = 'Disponible' THEN 1 END) AS Disponibles
+FROM 
+    CAMAS
+JOIN 
+    SALAS_UCI ON CAMAS.ID_salaUCI_FK = SALAS_UCI.ID_salaUCI
+WHERE 
+    CAMAS.ID_paciente_FK IS NOT NULL AND CAMAS.Estado = 'ocupado'
+    OR (CAMAS.ID_paciente_FK IS NULL AND CAMAS.Estado = 'Disponible')
+GROUP BY 
+    SALAS_UCI.ID_salaUCI;
