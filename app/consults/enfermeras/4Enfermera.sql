8ab6d573cfedb4170646f286aa5e7f10151eb85e
scorhenao <samcorhenao@gmail.com>
2024-06-23 11:59:41 -0500

    4ej enfermeras READY

--- app/consults/enfermeras/4Enfermera.sql
+++ app/consults/enfermeras/4Enfermera.sql
@@ -1,5 +1,5 @@
 - Dada una sala, conocer el médico que la dirige.
-SELECT SALAS_UCI.ID_salaUCI, MEDICOS.Nombre,MEDICOS.Apellidos/*Table where u are and table in which u wanna search*/
-FROM SALAS_UCI
-JOIN MEDICOS ON SALAS_UCI.ID_medico_FK = MEDICOS.ID_medico /*Compare if the FK is the same with the ID of the real table*/
-WHERE SALAS_UCI.ID_salaUCI = 1;/*The document that that we need*/
+SELECT MEDICOS.ID_medico, MEDICOS.Nombre, MEDICOS.Apellidos, SALAS_UCI.ID_salaUCI/*Table where u are and table in which u wanna search*/
+FROM MEDICOS /*from medicos we gonna search*/
+JOIN SALAS_UCI ON MEDICOS.ID_salaUCI_FK = SALAS_UCI.ID_salaUCI /*Compare if the FK is the same with the ID of the real table*/
+WHERE SALAS_UCI.ID_salaUCI = 20;/*The id of the sala_UCI*/
