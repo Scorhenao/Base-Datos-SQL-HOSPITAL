40b217bdce0414c8dca4ca59248eb0668ae61b5a
scorhenao <samcorhenao@gmail.com>
2024-06-23 10:57:49 -0500

    ej 1 enfermera ready

--- app/consults/enfermeras/1Enfermera.sql
+++ app/consults/enfermeras/1Enfermera.sql
@@ -1,1 +1,6 @@
 - Dado un número de cama, conocer los datos del paciente que se encuentra ACTUALMENTE allí.
+
+SELECT CAMAS.ID_cama, PACIENTES.Nombre, PACIENTES.Apellidos/*Table where u are and table in which u wanna search*/
+FROM CAMAS
+JOIN PACIENTES ON CAMAS.ID_paciente_FK = PACIENTES.ID_paciente /*Compare if the FK is the same with the ID of the real table*/
+WHERE CAMAS.ID_cama = 1;/*The ID that we need*/
