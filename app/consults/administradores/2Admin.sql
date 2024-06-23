d1d4650b704f4330129469bdd5b76f7603ea9f94
scorhenao <samcorhenao@gmail.com>
2024-06-23 14:12:22 -0500

    ej 2 admin READY

--- app/consults/administradores/2Admin.sql
+++ app/consults/administradores/2Admin.sql
@@ -1,1 +1,4 @@
 - Conocer el total de ingresos de un mes dado.
+SELECT COUNT(DISTINCT ID_paciente_FK) AS total_pacientes_junio_2024
+FROM RAZONES_DE_INGRESO
+WHERE MONTH(Fecha) = 6 AND YEAR(Fecha) = 2024;
