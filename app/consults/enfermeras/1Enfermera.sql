8d429334eff68cb3ad8b4f595b4ee9d41fcd9513
majolivero <majolivero28@gmail.com>
2024-06-26 10:52:21 -0500

    Solo cree un espacio

--- app/consults/enfermeras/1Enfermera.sql
+++ app/consults/enfermeras/1Enfermera.sql
@@ -4,3 +4,9 @@ SELECT CAMAS.ID_cama, PACIENTES.Nombre, PACIENTES.Apellidos/*Table where u are a
 FROM CAMAS
 JOIN PACIENTES ON CAMAS.ID_paciente_FK = PACIENTES.ID_paciente /*Compare if the FK is the same with the ID of the real table*/
 WHERE CAMAS.ID_cama = 1;/*The ID that we need*/
+
+
+
+
+
+
